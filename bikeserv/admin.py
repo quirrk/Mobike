4ae94554e535c88d521bcca9fc60586c7b856cb0
Felipe Herrera <felipe.ah14@gmail.com>
2018-11-14 00:39:25 -0300

    primer commit

---
+++ bikeserv/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Persona
+from .models import Bicicleta
+
+admin.site.register(Persona)
+admin.site.register(Bicicleta)
+
+
---
+++ bikeserv/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Persona (models.Model):
+    rut = models.CharField(max_length=10)
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=100)
+    comuna_opc = (
+        ('La Reina','La Reina'),
+        ('Providencia','Providencia'),
+        ('Ñuñoa','Ñuñoa'),
+    )
+    comuna = models.CharField(max_length=2, choices=comuna_opc)
+    cred = models.IntegerField()
+    def __str__(self):
+        return self.nombres
+
+class Bicicleta (models.Model):
+    cod = models.IntegerField()
+    def __str__(self):
+        return self.cod
