09e4bd655121411da0383a01e1d1b27b3467998d
Nicolas Reyes <nicolas.r.suarez@gmail.com>
2018-11-15 18:54:50 -0300

    Se añade navegación entre páginas

--- bikeserv/urls.py
+++ bikeserv/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('conocenos', views.quienessomos, name='conocenos'),
+    path('arrendar', views.arrendar, name='arrendar'),
 ]
--- bikeserv/views.py
+++ bikeserv/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request,'bikeserv/inicio.html',{})
+
+def quienessomos(request):
+    return render(request,'bikeserv/quienessomos.html', {})
+
+def arrendar(request):
+    return render(request,'bikeserv/arrendar.html', {})
