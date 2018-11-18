973c6cd06aa7073470774a286f3782f0a3e5141b
Felipe Herrera <felipe.ah14@gmail.com>
2018-11-17 22:37:37 -0300

    login required

--- bikeserv/views.py
+++ bikeserv/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def inicio(request):
     return render(request,'bikeserv/inicio.html',{})
@@ -6,5 +7,6 @@ def inicio(request):
 def quienessomos(request):
     return render(request,'bikeserv/quienessomos.html', {})
 
+@login_required
 def arrendar(request):
     return render(request,'bikeserv/arrendar.html', {})
