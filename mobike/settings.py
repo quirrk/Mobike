2db9ebcd3fe2f8c005b15c63fd835cee0f3f4362
Nicolas Reyes <nicolas.r.suarez@gmail.com>
2018-11-17 21:05:47 -0300

    Se agrega login & logout

--- mobike/settings.py
+++ mobike/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mobike.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+LOGIN_REDIRECT_URL = 'inicio'
+LOGOUT_REDIRECT_URL = 'inicio'
